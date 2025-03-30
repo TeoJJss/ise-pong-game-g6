3eb4650124d4a1204f4e66bdfcaa49dc92285a9f
Lim Heng Yang <lim.heng.yan@gmail.com>
2025-03-30 21:44:07 +0800

    hy changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # File path for audio
-whistle_sound = "" # when a player win
-pong_sound = "" # when collision
+whistle_sound = "./assets/audio/Winning.mp3" # when a player win
+pong_sound = "./assets/audio/pong sound.mp3" # when collision
 paddle_move_sound = "" # when paddle move
 ball_move_sound = "" # when ball move
 
